d993648b0300df02f148fd86d498f4d3143ea740
IhsanFackbo <159555124+IhsanFackbo@users.noreply.github.com>
2026-08-24 19:22:29 +0700

    Update storefront.tsx

--- app/storefront.tsx
+++ app/storefront.tsx
@@ -94,7 +94,7 @@ const products: Product[] = [
   name: "Preset Alight Motion 2",
   shortName: "Preset Semua ku di rayakan",
   price: 5_000,
-  image: "https://files.catbox.moe/gl0854.jpg",
+  image: "hhttps://h.uguu.se/ygmNgSci.jpg",
   description:
     "Preset Alight motion siap pakai, lengkap dan mudah diintegrasikan.",
   reviews: 0,
